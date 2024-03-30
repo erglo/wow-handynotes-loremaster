2157806515202b429518b1dab5a1e720c1daabc7
erglo <erglo.coder@gmail.com>
2024-03-30 19:25:19 +0100

    Update weekly quest IDs

--- Core.lua
+++ Core.lua
@@ -626,6 +626,7 @@ end
 QuestFilterUtils.weeklyQuests = {
     70750, 72068, 72373, 72374, 72375, 75259, 75859, 75860, 75861, 77254, 77976,  -- Dragonflight, "Aiding the Accord" quests
     78446, 78447, 78861,  -- Dragonflight, "Aiding the Accord" quests
+    80385, 80386, 80388, 80389,  -- Dragonflight, "Last Hurrah" quests
     66042,  -- Shadowlands, Zereth Mortis, "Patterns Within Patterns"
     63949,  -- Shadowlands, Korthia, "Shaping Fate"
     61332, 62861, 62862, 62863, -- Shadowlands, Covenant Sanctum (Kyrian), "Return Lost Souls" quests
